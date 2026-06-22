83395816c203eb940e9b6422f60e99bce013c157
pravaldave <pravaldave@gmail.com>
2026-06-22 16:06:43 +0530

    fix config.js for production

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE = "http://localhost:8000";
-export const USER_ID = "praval_test_account";
+export const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:8000';
+export const USER_ID = 'praval-default';
