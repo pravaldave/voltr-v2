38d31fa5e10f93004fe86b689b351a094744d4c0
pravaldave <pravaldave@gmail.com>
2026-06-25 12:44:53 +0530

    fix: use CRA env variable

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:8000';
+export const API_BASE = process.env.REACT_APP_API_BASE || 'http://localhost:8000';
 export const USER_ID = 'praval-default';
